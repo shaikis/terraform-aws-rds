5c4a2576915455d0de3dd35ba197896ef625cf0b
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-09-23 08:21:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,14 @@ output "rds_instance_endpoint" {
     value = aws_db_instance.main_rds_instance.endpoint
   
 }
+
+# configuration details in sigle output variable to pass app servers!!!
+output "db_config" {
+   value = {
+     user     = aws_db_instance.main_rds_instance.username
+     password = aws_db_instance.main_rds_instance.password
+     database = aws_db_instance.main_rds_instance.name
+     hostname = aws_db_instance.main_rds_instance.address
+     port     = aws_db_instance.main_rds_instance.port
+   }
+}
